e7b74c8df366c0019555e2d381e3d7c16ce760b7
Edwinsnp <156386495+Edwinsnp@users.noreply.github.com>
2024-02-11 18:36:30 -0600

    Add files via upload

--- list.js
+++ list.js
@@ -1,21 +1,50 @@
-document.addEventListener("DOMContentLoaded", () => {
-
+let currentIndex = 0;
+      let charactersData;
     
-    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        var template = document.getElementById("list-template");
-        var total = itemList.childElementCount + 1;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='nombre']").textContent = "Rick Sanchez";
-        clone.querySelector("[data-id='status']").textContent = "Vivo";
-        clone.querySelector("[data-id='especie']").textContent = "Humano";
-        clone.querySelector("[data-id='image']").src = "http://rickandmortyapi.com/api/character/avatar/1.jpeg";
-        
-        itemList.appendChild(clone);
-    });
-    document.querySelector(".btn.btn-light").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        itemList.replaceChildren();
-    });
-});
+      async function fetchCharacters() {
+        const response = await fetch('https://rickandmortyapi.com/api/character');
+        charactersData = await response.json();
+      
+      }
+    
+      async function agregar() {
+        if (!charactersData) {
+          await fetchCharacters();
+        }
+    
+        const characterListContainer = document.getElementById('character-list-container');
+    
+        if (currentIndex < charactersData.results.length) {
+          const character = charactersData.results[currentIndex];
+    
+          const listItem = document.createElement('div');
+          listItem.classList.add('character-card', 'position-relative');
+    
+          listItem.innerHTML = `
+            <div class="card">
+              <div class="number-badge">${currentIndex + 1}</div>
+              <img src="${character.image}" class="card-img-top" alt="Character Image">
+              <div class="card-body">
+                <u><h5 class="card-title">${character.name}</h5></u>
+                <p class="card-text">
+                  
+                  <span class="fw-bold">ID:</span> ${character.id}<br>
+                  <span class="fw-bold">Estado:</span> <span>${character.status}</span><br>
+                  <span class="fw-bold">Especie:</span> ${character.species}
+                </p>
+              </div>
+            </div>
+          `;
+    
+          characterListContainer.appendChild(listItem);
+          currentIndex++;
+        } else {
+          alert('No hay mas personajes');
+        }
+      }
+    
+      function limpiar() {
+        currentIndex = 0;
+        const characterListContainer = document.getElementById('character-list-container');
+        characterListContainer.innerHTML = '';
+      }
